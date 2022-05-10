4aa1ab37f644fc19450b445c04607269afea6bca
TayaHatum <21taya21@gmail.com>
2022-05-10 22:08:27 +0300

    added new options

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -2,7 +2,10 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -13,10 +16,15 @@ public class ApplicationManager {
     HelperSelect select;
     HelperJS js;
 
-
+//'C:\Users\YOUR_USER_NAME\AppData\Local\Google\Chrome\User Data\Default\Extensions'    win
+// /Users/YOUR_USER_NAME/Library/Application\ Support/Google/Chrome/Default/Extensions    mac
+//gighmmpiobklfepjocnamgkkbiglidom
 
     public void init() {
-        wd = new ChromeDriver();
+       // wd = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("load-extension=/Users/tayahatum/Tools/4.46.1_0");
+        wd = new ChromeDriver(options);
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wd.navigate().to("https://demoqa.com/");
@@ -25,6 +33,7 @@ public class ApplicationManager {
         windows = new HelperWindows(wd);
         select= new HelperSelect(wd);
         js = new HelperJS(wd);
+        returnAfterInstalledAdBlock();
 
 
     }
@@ -46,5 +55,13 @@ public class ApplicationManager {
 
     public HelperJS js() {
         return js;
+    }
+    private void returnAfterInstalledAdBlock() {
+
+        List<String> tabs = new ArrayList<>(wd.getWindowHandles());
+        wd.switchTo().window(tabs.get(1)).close();
+        wd.switchTo().window(tabs.get(0));
+
+
     }
 }
